f2bb8a7711ade8ee25cb90c725761bac08db9b1d
Mauricio Jost <mauricio.jost@amadeus.com>
2024-09-20 16:26:58 +0200

    Add repartitioning scenario and row-groups explanation

--- spark3/low-cpu-usage.not-enough-partitions-in-input.scala
+++ spark3/low-cpu-usage.not-enough-partitions-in-input.scala
@@ -6,24 +6,36 @@
 // COMMAND ----------
 
 /*
-This example shows how to identify and solve the problem of low CPU usage when reading a input dataframe that yields less
+This example shows how to identify and solve the problem of low CPU usage when reading an input dataframe that yields less
 partitions than the total amount of cores available.
 
 # Symptom
 
 For a given Job, the CPU usage is low for long periods of time (even when scaling up / out the cluster).
-In the Spark UI (live), the 'Executors' tab / 'Active Tasks' metrics much lower than the 'Cores' metric for a while.
-This happens in the first stage of a job.
+In the Spark UI (live), the 'Executors' tab / 'Active Tasks' metrics is much lower than the 'Cores' metric for a while.
+This happens in the first stage of a given job.
 
 # Explanation
 
-The Spark stage related to the input dataset read has not enough tasks to fit available cores, hence preventing the optimal
-distribution of the work across the available cores of the cluster.
+The Spark stage related to the input dataset read has not enough tasks to fit available cores, hence wasting
+CPU resources.
+
+Parquet files (and delta) are meant to be splitable. The unit of splitability is a row group. Each row group can be 
+assigned to a specific task. The row group size is defined by the setting parquet.block.size when writing the dataset.
+
+A single parquet file with multiple row groups can be processed in parallel right at the first Spark stage, however having multiple 
+tasks for a given row group is not possible (repartition is mandatory).
 
 In this example we take advantage of the splitability of .parquet files, which allows to have multiple partitions for a
 single file. The setting being used is 'spark.sql.files.maxPartitionBytes'. Mind that not all data storage formats
 are splittable by design. For instance .parquet is splittable, while .csv compressed with GZIP is not. In the case of
 consuming a non-splittable format you wll be forced to do a repartition of the input dataset to achieve similar speedups.
+
+The amount of row-groups in a given parquet file can be analysed using [parquet-tools](https://pypi.org/project/parquet-tools/), 
+option 'inspect'.
+
+Summary: if you have less tasks (pre-shuffle) than cores, try decreasing spark.sql.files.maxPartitionBytes (hopefully input files
+will have multiple row-groups that had been grouped together), if it does not work you are forced to do a .repartition().
 
 # What to aim for concretely
 
@@ -50,36 +62,56 @@ val tmpPath = "/tmp/perf-hikes/sandbox/" + UUID.randomUUID()
 spark.sparkContext.setJobDescription("Read CSV")
 val airports = spark.read.option("delimiter","^").option("header","true").csv(input)
 
+def showParitions(df: DataFrame) = df.withColumn("part_id", spark_partition_id()).groupBy("part_id").count().selectExpr("count(distinct(part_id)) as legit_partitions", "max(count) as max_records", "min(count) as min_records").show()
+
 // COMMAND ----------
 
 spark.sparkContext.setJobDescription("Initialize input table")
-airports.repartition(1).write.format("parquet").save(tmpPath + "/input") // create a table with a single partition (~500KB)
+spark.conf.set("parquet.block.size", 1024 * 1024) // size of the row-group is 1MB
+
+// single parquet file, roughly 4MB each (we create multiple input datasets to avoid warmup effects)
+Range(1,10).map(r => airports.withColumn("idx", lit(r))).reduce(_.union(_)).repartition(1).write.format("parquet").save(tmpPath + "/input1")
+Range(1,10).map(r => airports.withColumn("idx", lit(r))).reduce(_.union(_)).repartition(1).write.format("parquet").save(tmpPath + "/input2")
+Range(1,10).map(r => airports.withColumn("idx", lit(r))).reduce(_.union(_)).repartition(1).write.format("parquet").save(tmpPath + "/input3")
 
 // COMMAND ----------
 
-// Scenario with 1 partition, default spark.sql.files.maxPartitionBytes
-spark.sparkContext.setJobDescription("Read input (as 1 partition)")
-val df1 = spark.read.format("parquet").load(tmpPath + "/input")
-df1.toJavaRDD.partitions.size // what's the number of partitions calculated?
-df1.withColumn("part_id", spark_partition_id()).groupBy("part_id").count().show() // what's the distribution of records per partition?
-
-spark.sparkContext.setJobDescription("Write input (as 1 partition)")
-df1.write.format("noop").mode("overwrite").save()
+val expensiveProcessing = "sha(sha(sha(sha(sha(sha(sha(sha(sha(sha(sha(sha(sha(sha(sha(sha(sha(sha(sha(sha(sha(sha(wiki_link))))))))))))))))))))))"
 
 // COMMAND ----------
 
-// Scenario with multiple partitions, tweaked spark.sql.files.maxPartitionBytes
-spark.sparkContext.setJobDescription("Read input (as multiple partitions)")
-spark.conf.set("spark.sql.files.maxPartitionBytes", 1024*125)
-val df2 = spark.read.format("parquet").load(tmpPath + "/input")
-df2.toJavaRDD.partitions.size // what's the number of partitions calculated?
-df2.withColumn("part_id", spark_partition_id()).groupBy("part_id").count().show() // what's the distribution of records per partition?
+// Scenario with few partitions
+spark.sparkContext.setJobDescription("Read input (few partitions)")
+val df1 = spark.read.format("parquet").load(tmpPath + "/input1")
+showParitions(df1) // what's the distribution of records per partition?
 
-spark.sparkContext.setJobDescription("Write input (as multiple partitions)")
-df2.write.format("noop").mode("overwrite").save()
+spark.sparkContext.setJobDescription("Write input (few partitions)")
+df1.selectExpr(expensiveProcessing).write.format("noop").mode("overwrite").save()
+
+// COMMAND ----------
+
+// Scenario with many partitions
+spark.conf.set("spark.sql.files.maxPartitionBytes", 128*1024)
+spark.sparkContext.setJobDescription("Read input (many partitions)")
+val df2 = spark.read.format("parquet").load(tmpPath + "/input2")
+showParitions(df2) // what's the distribution of records per partition?
+
+spark.sparkContext.setJobDescription("Write input (many partitions)")
+df2.selectExpr(expensiveProcessing).write.format("noop").mode("overwrite").save()
 // It is possible to see the setting in the Spark UI, Tab 'SQL / DataFrame' then 'SQL / DataFrame Properties'
+
+// COMMAND ----------
+
+// Scenario with repartition
+spark.sparkContext.setJobDescription("Read input (repartitioned)")
+val df3 = spark.read.format("parquet").load(tmpPath + "/input3")
+spark.sparkContext.setJobDescription("Write input (repartitioned)")
+df3.repartition(4).selectExpr(expensiveProcessing).write.format("noop").mode("overwrite").save()
+
+// COMMAND ----------
 
 // In the 'Jobs' tab, the 'save' action for each scenario is represented by 1 job 'Write input...'
 // Mind that those jobs are very different:
-// - for 'Write input (as 1 partition)' job with default settings: 1 task, longer duration
-// - for 'Write input (as multiple partitions)' with tweaked ones: 4 tasks, shorter duration
+// - for 'Write input (few partitions)': few tasks, longer duration overall
+// - for 'Write input (many partitions)': many tasks, shorter duration overall
+// - for 'Write input (repartitioned)': many tasks, shorter duration overall
